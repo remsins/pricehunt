c63daff764a4a1fbc802e974a398cbb804bbc726
Srishti singh <singhsrishti1934@gmail.com>
2025-05-20 13:11:48 +0530

    components added

---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <header className='w-full'>
+      <nav className='nav'>
+        <Link href='/' className='flex items-center gap-1'>
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt='logo'
+          />
+          <p className='nav-logo'>
+            Price
+          </p>
+        </Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
